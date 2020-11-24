4700313ed276f6ccd6c233f4d5e81c9ab508da79
Esa Varemo <esa@kuivanto.fi>
2020-11-24 19:40:36 +0000

    Convert to stm32l0, set up IO pins

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 
 #![deny(unsafe_code)]   //  Don't allow unsafe code in this file.
-#![deny(warnings)]      //  If the Rust compiler generates a warning, stop the compilation with an error.
+//#![deny(warnings)]      //  If the Rust compiler generates a warning, stop the compilation with an error.
 #![no_main]             //  Don't use the Rust standard bootstrap. We will provide our own.
 #![no_std]              //  Don't use the Rust standard library. We are building a binary that can run on its own.
 
@@ -10,31 +10,54 @@ use cortex_m_semihosting::hprintln;                     //  For displaying messa
 use panic_semihosting as _;
 
 use embedded_hal::digital::v2::OutputPin;
-use stm32f1xx_hal::{delay::Delay, pac, prelude::*};
+use stm32l0xx_hal::{delay::Delay, pac, prelude::*, rcc::Config};
 
 #[entry]
 fn main() -> ! {
     hprintln!("Hello, world!").unwrap();
 
-    // Core peripherals
     let cp = cortex_m::Peripherals::take().unwrap();
-
-    // Device peripherals
     let dp = pac::Peripherals::take().unwrap();
 
-    let mut flash = dp.FLASH.constrain();
-    let mut rcc = dp.RCC.constrain();
+    //let mut flash = dp.FLASH.constrain();
+    //let mut rcc = dp.RCC.constrain();
 
     // Freeze the configuration of all the clocks in the system and store the frozen frequencies in
     // `clocks`
-    let clocks = rcc.cfgr.freeze(&mut flash.acr);
+    //let clocks = rcc.cfgr.freeze(&mut flash.acr);
 
-    // Configure gpio C pin 13 as a push-pull output. The `crh` register is passed to the function
-    // in order to configure the port. For pins 0-7, crl should be passed instead.
-    let mut gpioc = dp.GPIOC.split(&mut rcc.apb2);
-    let mut led = gpioc.pc13.into_push_pull_output(&mut gpioc.crh);
+    let mut rcc = dp.RCC.freeze(Config::hsi16());
 
-    let mut delay = Delay::new(cp.SYST, clocks);
+    let mut gpioa = dp.GPIOA.split(&mut rcc);
+    let mut gpiob = dp.GPIOB.split(&mut rcc);
+    let mut gpioc = dp.GPIOC.split(&mut rcc);
+
+    let mut M1_IN_A = gpioa.pa10.into_push_pull_output();
+    let mut M1_IN_B = gpiob.pb5.into_push_pull_output();
+    let mut M1_EN = gpiob.pb10.into_push_pull_output();
+    let mut M1_PWM = gpioc.pc7.into_push_pull_output();
+
+    let mut M2_IN_A = gpioa.pa8.into_push_pull_output();
+    let mut M2_IN_B = gpioa.pa9.into_push_pull_output();
+    let mut M2_EN = gpioa.pa6.into_push_pull_output();
+    let mut M2_PWM = gpiob.pb6.into_push_pull_output();
+    
+    // M1_CS = pa0
+    // M2_CS = pa1
+
+    M1_EN.set_high().unwrap();
+    M1_PWM.set_high().unwrap();
+
+    M2_EN.set_high().unwrap();
+    M2_PWM.set_high().unwrap();
+
+    M1_IN_A.set_high().unwrap();
+    M2_IN_B.set_high().unwrap();
+
+    let mut led = gpioc.pc13.into_push_pull_output();
+    let mut delay = cp.SYST.delay(rcc.clocks);
+
+    //loop {}
 
     // Wait for the timer to trigger an update and change the state of the LED
     loop {
